318ebe0da0cc1cce68c1bdb57bc944df1fea6f42
katieschluth <41099719+katieschluth@users.noreply.github.com>
2018-07-18 16:51:43 -0400

    Update read_ascaris_data.py

--- read_ascaris_data.py
+++ read_ascaris_data.py
@@ -8,4 +8,4 @@ Created on Thu Jul 12 16:55:53 2018
 import pandas as pd
 helminths = pd.read_excel("ascaris_lumbricoides_raw_data.xlsx", sheetname = "ascaris_lumbricoides_raw_data")
 print(helminths.head(506))
-#This is definitely not the most effective way to get all the data to print, but I can't figure out the code to just read the whole sheet. Thoughts?
+#This is definitely not the most effective way to get all the data to print, but I can't figure out the code to just read the whole sheet. I feel like this should be simple. Thoughts?
